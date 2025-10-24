6d1ab97299edccf08b21ca9b9aca445bf896e18d
AccessCoder <info.unaddict@gmail.com>
2025-10-24 16:31:52 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(List.of("Max", "Maxi", "Maxim", "Maximilian"));
+
+        names.stream()
+                .map(String::toUpperCase) //Method Ref.
+                .sorted()
+                .filter(name -> name.length() < 5) //Lambda
+                .forEach(System.out::println);
+
+        names.stream()
+                .map(String::toUpperCase) //Method Ref.
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String name) {
+                        return name.length() < 5;
+                    }
+                }) //Lambda
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+    }
+}
